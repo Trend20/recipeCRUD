8145f66fa129c787d299cd392693999d136f5422
Trend20 <enockomondi305@gmail.com>
2024-09-02 10:38:07 +0300

    a simple go api

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Todo struct {
+	ID          string `json:"id"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+}
+
+var todos []Todo
+
+func main() {
+
+	//	define routes
+	http.HandleFunc("/todos", getTodos)
+
+	//	start the server and listen
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+// define getTodos controller
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(todos)
+	if err != nil {
+		return
+	}
+}
